32ceba0ba5b838306d07e152d735b11ad7ae25d2
Jaxson Pahukula <jaxpahu@gmail.com>
2025-02-22 01:59:14 -0500

    got judge loading in

--- host/src/App.tsx
+++ host/src/App.tsx
@@ -1,10 +1,24 @@
-import { FreeCamera, Vector3, HemisphericLight, MeshBuilder, Mesh, Scene } from "@babylonjs/core";
+import {
+	FreeCamera,
+	Vector3,
+	HemisphericLight,
+	MeshBuilder,
+	Mesh,
+	Scene,
+	LoadAssetContainerAsync,
+} from "@babylonjs/core";
+import { registerBuiltInLoaders } from "@babylonjs/loaders/dynamic";
 import SceneComponent from "./components/SceneComponent";
 
-let box: Mesh;
+let judge: Mesh;
 
-const onSceneReady = (scene: Scene) => {
+async function onSceneReady(scene: Scene) {
 	console.debug("Initializing scene");
+
+	registerBuiltInLoaders();
+
+	// gotta be RH for glb
+	scene.useRightHandedSystem = true;
 
 	const camera = new FreeCamera("camera1", new Vector3(0, 5, -10), scene);
 	camera.setTarget(Vector3.Zero());
@@ -12,29 +26,32 @@ const onSceneReady = (scene: Scene) => {
 	const canvas = scene.getEngine().getRenderingCanvas();
 
 	// allows for control with mouse
-	// camera.attachControl(canvas, true);
+	camera.attachControl(canvas, true);
 
 	// This creates a light, aiming 0,1,0 - to the sky (non-mesh)
-	const light = new HemisphericLight("light", new Vector3(0.5, 1, 0), scene);
+	const light = new HemisphericLight("light", new Vector3(-1, 3, 0), scene);
 
 	light.intensity = 0.9;
 
-	box = MeshBuilder.CreateBox("box", { size: 2 }, scene);
+	{
+		// loading judge model
+		const container = await LoadAssetContainerAsync("/assets/judge.glb", scene);
+		judge = container.meshes[0] as Mesh;
+		container.addAllToScene();
+	}
 
-	box.position.y = 1;
+	//box = MeshBuilder.CreateBox("box", { size: 2 }, scene);
+	// box.position.y = 1;
 
 	MeshBuilder.CreateGround("ground", { width: 6, height: 6 }, scene);
 
 	console.debug("Scene initialization done");
-};
+}
 
 // runs once per frame
 const onRender = (scene: Scene) => {
-	if (box !== undefined) {
-		const deltaTimeInMillis = scene.getEngine().getDeltaTime();
-
-		const rpm = 10;
-		box.rotation.y += (rpm / 60) * Math.PI * 2 * (deltaTimeInMillis / 1000);
+	if (typeof judge != "undefined") {
+		judge.addRotation(0, 0.01, 0);
 	}
 };
 
